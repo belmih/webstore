2efbcb3c28bfaf250826b592b3cac6d5d6c2e8b6
mih <belmd@yandex.ru>
2017-09-08 15:53:28 +0300

    Connect to DataBase!

--- src/main/java/ru/belmih/webstore/HomeController.java
+++ src/main/java/ru/belmih/webstore/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +22,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	HomeController(DataSource dataSource){
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -35,5 +45,14 @@ public class HomeController {
 		
 		return "home";
 	}
+
+    
+	@RequestMapping(value = "/test/", method = RequestMethod.GET)
+	public String base() {
+		logger.info("In base();");
+		jdbcTemplate.execute("CREATE TABLE customers1(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+		return "base";
+	}
+	
 	
 }
